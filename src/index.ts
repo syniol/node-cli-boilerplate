c124780dec017fbf6e6614de8299474faa282738
Hadi T <tajallaei@gmail.com>
2022-01-16 17:55:01 +0000

    cosmetic changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { main } from './lib'
 
+
 ;(async () => {
     await main()
 })()
--- src/lib/execute.ts
+++ src/lib/execute.ts
@@ -1,14 +1,16 @@
 import { join } from 'path'
+
 import { loadFunction } from './loader'
 
+
 export async function main() {
-    const functionTemplate: { functions: string[] } = await import(
+    const { functions }: { functions: string[] } = await import(
         join(__dirname, '..', '..', 'template.json')
     )
     const args = process.argv.slice(2)
 
     const tasks: Promise<void>[] = []
-    for (const functionName of functionTemplate.functions) {
+    for (const functionName of functions) {
         tasks.push((await loadFunction(functionName)).default(args))
     }
 
